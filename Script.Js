030e958541edaa9da0d8e02a2b21c249fadcae9d
marialopessantos03-sudo <maria.lopes.santos03@escola.pr.gov.br>
2026-06-18 11:26:52 -0300

    .

--- Script.Js
+++ Script.Js
@@ -1,1 +1,15 @@
+function filtrar(categoria) {
+  const cards = document.querySelectorAll('.card');
 
+  cards.forEach(card => {
+    if (categoria === 'todos') {
+      card.style.display = 'block';
+    } else {
+      if (card.classList.contains(categoria)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    }
+  });
+}
